e1df3156abf08770a9dfd192bb8d9840b0dba649
James Johnson <jamesjhnsn1995@gmail.com>
2023-01-23 16:34:31 -0800

    made a prop

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 import './Box.css'
 
-export const Box = () => {
+export const Box = ({value, onClick}) => {
+  const style = value === 'X' ? 'box x' : 'box o'
+
   return (
-    <div>Hello</div>
+    <button className={style} onClick={onClick}>{value}</button>
   )
 }
